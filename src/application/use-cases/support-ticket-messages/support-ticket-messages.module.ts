46e264b3bbf272e3511b91edd7643e7c41f9b09c
Igor Costa <igorsantoscosta@gmail.com>
2024-02-22 20:46:44 -0300

    feat: create find all messages by ticket use case

--- src/application/use-cases/support-ticket-messages/support-ticket-messages.module.ts
+++ src/application/use-cases/support-ticket-messages/support-ticket-messages.module.ts
@@ -2,16 +2,22 @@ import { Module } from '@nestjs/common';
 import { SupportTicketMessagesRepository } from '@/application/repositories/support-ticket-messages.repository';
 import { PrismaSupportTicketMessagesRepository } from '@/application/repositories/implementations/prisma-support-ticket-messages.repository';
 import { CreateMessageFromSupportTicketService } from './create-message-from-support-ticket.service';
+import { FindAllMessagesByTicketService } from './find-all-messages-by-ticket.service';
 
 @Module({
   imports: [],
   providers: [
     CreateMessageFromSupportTicketService,
+    FindAllMessagesByTicketService,
     {
       provide: SupportTicketMessagesRepository,
       useClass: PrismaSupportTicketMessagesRepository,
     },
   ],
-  exports: [CreateMessageFromSupportTicketService],
+  exports: [
+    CreateMessageFromSupportTicketService,
+    FindAllMessagesByTicketService,
+    SupportTicketMessagesRepository,
+  ],
 })
 export class SupportTicketMessagesModule {}
