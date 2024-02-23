e23fc3ac22d1f77c8683e40b65c615f9e59a0360
Igor Costa <igorsantoscosta@gmail.com>
2024-02-23 20:09:11 -0300

    test: create test for delete support ticket service

---
+++ src/application/use-cases/support-tickets/delete-unique-support-ticket.service.spec.ts
@@ -0,0 +1,48 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { SupportTicketsRepository } from '@/application/repositories/support-tickets.repository';
+import { InMemorySupportTicketsRepository } from '@/application/repositories/implementations/in-memory-support-tickets.repository';
+import { randomUUID } from 'crypto';
+import { DeleteUniqueSupportTicketService } from './delete-unique-support-ticket.service';
+
+describe('Delete Support Ticket Use Case', () => {
+  let sut: DeleteUniqueSupportTicketService;
+  let supportTicketsRepository: SupportTicketsRepository;
+
+  beforeEach(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      providers: [
+        DeleteUniqueSupportTicketService,
+        {
+          provide: SupportTicketsRepository,
+          useClass: InMemorySupportTicketsRepository,
+        },
+      ],
+    }).compile();
+
+    sut = moduleRef.get(DeleteUniqueSupportTicketService);
+    supportTicketsRepository = moduleRef.get(SupportTicketsRepository);
+  });
+
+  it('should be able to delete support ticket', async () => {
+    const userId = randomUUID();
+
+    const supportTicket = await supportTicketsRepository.create({
+      title: 'Ticket of test 01',
+      description: 'Description of support ticket 01',
+      creator: { connect: { id: userId } },
+    });
+
+    const findAllSupportTicketsBeforeDeletion =
+      await supportTicketsRepository.findAll({ page: 1, pageSize: 10 });
+
+    expect(findAllSupportTicketsBeforeDeletion).toHaveLength(1);
+
+    await sut.execute(supportTicket.id);
+
+    const findAllSupportTicketsAfterDeletion =
+      await supportTicketsRepository.findAll({ page: 1, pageSize: 10 });
+
+    expect(findAllSupportTicketsAfterDeletion).toHaveLength(0);
+  });
+});
